135af4438ebfb01edf505e401c2c5550bb76a873
RAmeshDustrise123 <ramesh@dustrise.com>
2013-03-28 13:09:33 +0530

    Added subcategory class to main project

---
+++ StoreMall/SubCatagory.h
@@ -0,0 +1,13 @@
+//
+//  SubCatagory.h
+//  StoreApp
+//
+//  Created by dustrise on 25/03/13.
+//  Copyright (c) 2013 dustrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubCatagory : UITableViewController
+
+@end
