0bbaa3f58d0ec5f0e4c841014a47bcc01d127fc8
suresh <suresh@dustrise.com>
2013-04-02 12:51:04 +0530

    Added button

---
+++ StoreMall/Login.h
@@ -0,0 +1,20 @@
+//
+//  Login.h
+//  StoreApp
+//
+//  Created by dustrise on 25/03/13.
+//  Copyright (c) 2013 dustrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Login : UIViewController
+{
+    
+    //comments
+    
+    IBOutlet UIButton *btn;
+    
+}
+
+@end
---
+++ StoreMall/Login.m
@@ -0,0 +1,38 @@
+//
+//  Login.m
+//  StoreApp
+//
+//  Created by dustrise on 25/03/13.
+//  Copyright (c) 2013 dustrise. All rights reserved.
+//
+
+#import "Login.h"
+
+@interface Login ()
+
+@end
+
+@implementation Login
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
